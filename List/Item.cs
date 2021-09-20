5ad866e4c16586ea129cc85808d06701bd41a6f9
no_name <nikiforowegor98@mail.ru>
2021-09-20 21:52:53 +0700

    first commit

---
+++ List/Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class Item<T>
+    {
+        /// <summary>
+        /// Хранимые данные.
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// Следующий элемент связного списка.
+        /// </summary>
+        public Item<T> Next { get; set; }
+
+        /// <summary>
+        /// Создание нового экземпляра связного списка.
+        /// </summary>
+        /// <param name="data"> Сохраняемые данные. </param>
+        public Item(T data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            Data = data;
+        }
+
+        /// <summary>
+        /// Приведение объекта к строке.
+        /// </summary>
+        /// <returns> Хранимые данные. </returns>
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            foreach (int item in list) 
+            {
+                Console.WriteLine(item);
+            }
+
+            list.Clear();
+
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
