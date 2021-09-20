736728bd4e68ac9b82ffabc955db65bf8cd241aa
no_name <nikiforowegor98@mail.ru>
2021-09-20 22:07:28 +0700

    list

--- List/Item.cs
+++ List/Item.cs
@@ -6,20 +6,10 @@ namespace LinkedList
 {
     public class Item<T>
     {
-        /// <summary>
-        /// Хранимые данные.
-        /// </summary>
         public T Data { get; set; }
 
-        /// <summary>
-        /// Следующий элемент связного списка.
-        /// </summary>
         public Item<T> Next { get; set; }
 
-        /// <summary>
-        /// Создание нового экземпляра связного списка.
-        /// </summary>
-        /// <param name="data"> Сохраняемые данные. </param>
         public Item(T data)
         {
             if (data == null)
@@ -30,10 +20,6 @@ namespace LinkedList
             Data = data;
         }
 
-        /// <summary>
-        /// Приведение объекта к строке.
-        /// </summary>
-        /// <returns> Хранимые данные. </returns>
         public override string ToString()
         {
             return Data.ToString();
--- List/Program.cs
+++ List/Program.cs
@@ -6,24 +6,6 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>();
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            list.Add(4);
-            list.Add(5);
-
-            foreach (int item in list) 
-            {
-                Console.WriteLine(item);
-            }
-
-            list.Clear();
-
-            foreach (int item in list)
-            {
-                Console.WriteLine(item);
-            }
 
         }
     }
